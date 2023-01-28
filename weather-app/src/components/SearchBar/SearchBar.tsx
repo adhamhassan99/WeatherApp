5b4389d9fe142f8ef2b8f261bb5ffd979a2b7f3b
adhamhassan99 <49523561+adhamhassan99@users.noreply.github.com>
2023-01-28 20:50:00 +0200

    autocomplete

--- weather-app/src/components/SearchBar/SearchBar.tsx
+++ weather-app/src/components/SearchBar/SearchBar.tsx
@@ -7,9 +7,11 @@ import { useDispatch, useSelector } from "react-redux";
 import { setQueryKey } from "../../slices/temperatureSlice";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import { Autocomplete } from "@mui/material";
 export default function SearchBar({}: Props) {
   const { queryKey } = useSelector((state) => state.temperature);
   const [value, setValue] = useState(queryKey);
+  const [options, setOptions] = useState([]);
   const dispatch = useDispatch();
   // dispatch(setQueryKey(event.target.value))
   const handleEnter = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -25,27 +27,52 @@ export default function SearchBar({}: Props) {
         `http://api.weatherapi.com/v1/search.json?key=f87e3f6eaec34528a9902910231401&q=${value}`
       ),
     onSuccess: (data) => {
-      console.log(data);
+      console.log(data.data);
+      setOptions(
+        data.data.map((item) => `${item.name},${item.region},${item.country}`)
+      );
     },
   });
 
-  const handleChange = (searchString: string) => {
-    setValue(searchString);
-  };
-
   return (
-    <div>
-      <div className="flex border-secondaryText items-center gap-3">
-        <FontAwesomeIcon icon={faMagnifyingGlass} />
-        <input
-          value={value}
-          onChange={(event) => handleChange(event.target.value)}
-          onKeyDown={(event) => handleEnter(event)}
-          type="text"
-          className="outline-none placeholder:text-black"
-          placeholder="Search for places ..."
-        />
-      </div>
-    </div>
+    <>
+      <Autocomplete
+        filterOptions={(x) => x}
+        value={value}
+        onChange={(event: any, newValue: string | null) => {
+          setValue(newValue);
+        }}
+        inputValue={value}
+        onInputChange={(event, newInputValue) => {
+          setValue(newInputValue);
+        }}
+        onKeyDown={(event) => handleEnter(event)}
+        sx={{
+          display: "inline-block",
+          "& input": {
+            width: 200,
+            bgcolor: "background.paper",
+            color: (theme) =>
+              theme.palette.getContrastText(theme.palette.background.paper),
+          },
+        }}
+        id="custom-input-demo"
+        options={options}
+        renderInput={(params) => (
+          <div
+            ref={params.InputProps.ref}
+            className="flex border-secondaryText items-center gap-3"
+          >
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+            <input
+              {...params.inputProps}
+              type="text"
+              className="outline-none placeholder:text-black"
+              placeholder="Search for places ..."
+            />
+          </div>
+        )}
+      />
+    </>
   );
 }
