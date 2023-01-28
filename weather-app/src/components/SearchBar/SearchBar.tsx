b9a13782f7027d834d4dc553a4fcfba466e7b93f
adhamhassan99 <49523561+adhamhassan99@users.noreply.github.com>
2023-01-28 20:16:57 +0200

    adding query logic

--- weather-app/src/components/SearchBar/SearchBar.tsx
+++ weather-app/src/components/SearchBar/SearchBar.tsx
@@ -5,6 +5,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { useDispatch, useSelector } from "react-redux";
 import { setQueryKey } from "../../slices/temperatureSlice";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import axios from "axios";
 export default function SearchBar({}: Props) {
   const { queryKey } = useSelector((state) => state.temperature);
   const [value, setValue] = useState(queryKey);
@@ -16,13 +18,28 @@ export default function SearchBar({}: Props) {
     }
   };
 
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["autoComp", value],
+    queryFn: () =>
+      axios.get(
+        `http://api.weatherapi.com/v1/search.json?key=f87e3f6eaec34528a9902910231401&q=${value}`
+      ),
+    onSuccess: (data) => {
+      console.log(data);
+    },
+  });
+
+  const handleChange = (searchString: string) => {
+    setValue(searchString);
+  };
+
   return (
     <div>
       <div className="flex border-secondaryText items-center gap-3">
         <FontAwesomeIcon icon={faMagnifyingGlass} />
         <input
           value={value}
-          onChange={(event) => setValue(event.target.value)}
+          onChange={(event) => handleChange(event.target.value)}
           onKeyDown={(event) => handleEnter(event)}
           type="text"
           className="outline-none placeholder:text-black"
